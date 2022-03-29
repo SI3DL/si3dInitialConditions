41b9be4994b55ab017c11302c2e366fb50b0c608
savalma <savalbuena@ucdavis.edu>
2022-03-29 07:50:22 -0700

    The file now includes the edition of the si3d_inp.txt file to use the specifications used when creating the si3d_init.txt and the si3d_layer.txt

--- InitialConditions.py
+++ InitialConditions.py
@@ -2,9 +2,9 @@
 # This code creates the initial conditions for numerical simulations using SI3D.
 # Copy Right Sergio A. Valbuena 2021
 # UC Davis - TERC
-# February 2021
+# August 2021
 
-# For a CTD profile, it is recommended that the user modifies the code within the if statement for TempProf = 'variable'. Where one can load the CTD profile and assign to the corresponding variables z_CTD and T_CTD to run the code.
+# For a CTD profile, it is recommended that the user modifies the code within the if statement for TempProf == 'variable'. Where one can load the CTD profile and assign to the corresponding variables z_CTD and T_CTD to run the code.
 
 # Library Import
 import sys
@@ -25,50 +25,69 @@ sys.path.append(FuncPath)
 from si3dInputs import initCond4si3d
 del root, user, func, FuncPath
 
+plt.close('all')
 # --------------------- User variables declaration ----------------------------
-root = "S:/"
-PathProject = "si3D/"
+root = 'S:/si3D/'
+PathProject = 'LakeMassawippi/'
 # Chose the name of folder for the simulation
-SimFolder = 'L10_H100_W15_N10_f40'
+SimFolder = 'Lake_R70'
 # Chose path to save bathymetry file
 PathSave = root+PathProject+SimFolder
 # Specify the name of the Lake
-LakeName = 'Stratification 1 for canonical basin'
+LakeName = 'Stratification for Lake Massawippi'
 # Specify the start date of the simulation
-SimStartDate = '2018-05-26 00:00:00'
-TimeZone = 'America/Los_Angeles'
+SimStartDate = '2019-06-01 04:00:00' # IN UTC BUT CONSIDER SIMS ARE IN LOCAL TIME
+TimeZone = 'America/Toronto'
 # Define depth of lake
-H = 100 #[m] deep
+H = 85 #[m] deep
 # Chosse number of tracers
-NTracers = 0
+NTracers = 2
 # Chose the type of thickness in layers and the profile of temperature in the vertical direction. Options are 'constant' and 'variable'
-DeltaZ = 'constant'
+DeltaZ = 'variable'
 TempProf = 'variable'
-# DeltaZ = 'variable'
-# TempProf = 'variable'
 
 if DeltaZ == 'constant':
     dz = 1 #[m] constant thickness of layers
 elif DeltaZ == 'variable':
-    # Chose the layer thickness method. Current options are: 'sbconc' and 'exp' where the first concentrates the specified minimum thickess at surface and bottom, and the latter has the finest layers at the surface.
-    spacingMethod = 'exp'
+    # Chose the layer thickness method. Current options are: 'sbconc', 'surfvarBotconsta' and 'exp' where the first concentrates the specified minimum thickess at surface and bottom, and the latter has the finest layers at the surface.
+    spacingMethod = 'surfvarBotconsta'
     # Initial dx of top and bottom layers. Where the bottom is only used if sbconc method is used.
-    dz0s = 0.1
-    dz0b = 0.1
+    dz0s = 0.2
+    dz0b = 0.5
     # Specify constant spacing as layer thickness increases
-    dzxs = 1.03
+    dzxs = 1.02
     dzxb = 1.03
     # H/n will be the depth at which the thickness of layers start to decrease until reaching the bottom thicness of dz0b
     n = 3
+    Hn = H/1.5
+    dzc = 2 # This value will be saved in si3d_inp.txt
+
 if TempProf == 'constant':
     Tc = 15 #[C] Temperature of whole water column
 elif TempProf == 'variable':
-    PathFile = "G:/My Drive/Lake_Tahoe/Projects/Upwelling_3DModel/CTD_data/Processed_0"
-    FileName = 'LakeTahoe_CTD.csv'
+    PathFile = 'G:/My Drive/Lake_Tahoe/Projects/Lake_Massawippi/Data_Forcing/02_ProcessStep/01_TData'
+
+if NTracers != 0:
+    PathTr = 'G:/My Drive/Lake_Tahoe/Projects/Lake_Massawippi/Data_Forcing/02_ProcessStep/01_TData'
+    z_Tr = np.reshape(np.array([0,10,20,30,40,50,60]),(7,1))
+    z_Tr = np.repeat(z_Tr,2,axis=1)
+    conc_Tr = np.reshape(np.array([10,9.5,8,7,6,5,4]),(7,1))
+    conc_Tr = 0.5*np.repeat(conc_Tr,2,axis=1)
+    conc_Tr[:,0] *= 2
+elif NTracers == 0:
+    z_Tr = 0
+    conc_Tr = 0
+
+# -------------- Beginning of code to create initial condition file --------------------
+# To load the data and create variables for creating initial conditions file.
+if TempProf == 'variable':
+    StartDateS = SimStartDate[0:10]+'_'+SimStartDate[11:13]+'Hrs'
+    print(StartDateS)
+    FileName = 'CTD_' + StartDateS + '.npy'
     os.chdir(PathFile)
-    data = pd.read_csv(FileName)
-    z_CTD = data['Depth'].to_numpy()
-    T_CTD = data['T'].to_numpy()
+    data = np.load(FileName,allow_pickle=True).item()
+    z_CTD = data['z']
+    T_CTD = data['T']
     del data
     if np.max(z_CTD) < H/2:
         z_CTD = np.append(z_CTD,(H/2,H))
@@ -76,17 +95,48 @@ elif TempProf == 'variable':
     elif np.max(z_CTD) > H/2 and np.max(z_CTD) < H:
         z_CTD = np.append(z_CTD,H)
         T_CTD = np.append(T_CTD,T_CTD[-1])
+    if np.min(z_CTD) > 0.5:
+        z_CTD = np.append(0,z_CTD)
+        T_CTD = np.append(T_CTD[0],T_CTD)
 
-plt.plot(T_CTD,-z_CTD)
-# -------------- Beginning of code to create initial condition file --------------------
+# plt.plot(T_CTD,-z_CTD)
 if DeltaZ == 'constant':
     if TempProf == 'constant':
-        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave,NTracers,H,dz,Tc)
+        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave, NTracers,z_Tr=z_Tr,conc_Tr=conc_Tr,H=H,dz=dz,Tc=Tc)
     elif TempProf == 'variable':
-        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave,NTracers,H,dz,z_CTD,T_CTD)
+        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave, NTracers,z_Tr=z_Tr,conc_Tr=conc_Tr,H=H,dz=dz, z_CTD=z_CTD,T_CTD=T_CTD)
 elif DeltaZ == 'variable':
     if TempProf == 'constant':
-        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave,NTracers,H,Tc,spacingMethod,dz0s,dz0b,dzxs,dzxb,n)
+        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave, NTracers,z_Tr=z_Tr,conc_Tr=conc_Tr, H=H,Tc=Tc,spacingMethod=spacingMethod, dz0s=dz0s,dz0b=dz0b,dzxs=dzxs,dzxb=dzxb,n=n,Hn=Hn,dzc=dzc)
     elif TempProf == 'variable':
-        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave,NTracers,H,z_CTD,T_CTD,spacingMethod,dz0s,dz0b,dzxs,dzxb,n)
+        [T,z] = initCond4si3d(LakeName,SimStartDate,DeltaZ,TempProf,PathSave, NTracers,z_Tr=z_Tr,conc_Tr=conc_Tr, H=H,z_CTD=z_CTD,T_CTD=T_CTD, spacingMethod=spacingMethod,dz0s=dz0s,dz0b=dz0b,dzxs=dzxs,dzxb=dzxb,n=n,Hn=Hn,dzc=dzc)
+
+fidinp = open('si3d_inp.txt',"r+")
+lines = fidinp.readlines()
+
+if DeltaZ == 'constant':
+    lines[18] = 'idz          !       '+str(dz)+'.0          ! Cell size (m) in vertical\n'
+    lines[23] = 'ibathf       !        0           ! How bathy file is read 0(General) 1(SDWSC) 2(SHR)\n'
+    H = -1*z[-1] + dz/2
+    H = np.ceil(H)
+    H = str(H)
+    if len(H) == 4:
+        lines[14] = 'zl           !      ' + H + '          !   "    "    "    (m) in vertical\n'
+    elif len(H) == 5:
+        lines[14] = 'zl           !     ' + H + '          !   "    "    "    (m) in vertical\n'
+else:
+    lines[18] = 'idz          !       '+str(dzc)+'.0          ! Cell size (m) in vertical\n'
+    lines[23] = 'ibathf       !        -1          ! How bathy file is read 0(General) 1(SDWSC) 2(SHR)\n'
+    H = -1*z[-1]
+    H = np.ceil(H)
+    H = str(H)
+    if len(H) == 4:
+        lines[14] = 'zl           !      ' + H + '          !   "    "    "    (m) in vertical\n'
+    elif len(H) == 5:
+        lines[14] = 'zl           !     ' + H + '          !   "    "    "    (m) in vertical\n'
+# To write the new lines into input file
+fidinp = open('si3d_inp.txt','w+')
+fidinp.writelines(lines)
+fidinp.close()
+
 plt.show()
